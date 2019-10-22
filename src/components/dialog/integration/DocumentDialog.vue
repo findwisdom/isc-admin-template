d15f8fc67224b6d1b425e38078f1424cbf126127
Yvonne <15201255456@163.com>
2019-10-22 15:20:45 +0800

    feat: 文档完成

---
+++ src/components/dialog/integration/DocumentDialog.vue
@@ -0,0 +1,109 @@
+<template>
+    <app-dialog
+        :visible.sync="visible2"
+        @close="onClose"
+        @open="onOpen"
+        @sure="onSure"
+        :loading="loading"
+        width="400px"
+    >
+        <template v-slot:title>
+            {{ actionName }}
+        </template>
+
+        <template v-slot:default>
+            <el-form ref="form" :model="form" :rules="rules" label-width="60px" size="mini">
+                <el-form-item label="名称" prop="name">
+                    <el-input v-model="form.name" maxlength="20"></el-input>
+                </el-form-item>
+                <el-form-item label="简介" prop="description">
+                    <el-input type="textarea" :rows="2" v-model="form.description" maxlength="150"></el-input>
+                </el-form-item>
+                <el-form-item label="链接" prop="link">
+                    <el-input v-model="form.link"></el-input>
+                </el-form-item>
+            </el-form>
+        </template>
+    </app-dialog>
+</template>
+
+<script>
+import AppDialog from '@/components/app/AppDialog';
+import { success, error } from '@/utils/message';
+import validation from '@/validations/ducument';
+import { createUpdateDocument } from '@/services/integration';
+export default {
+    name: 'DocumentDialog',
+    components: {
+        AppDialog
+    },
+    props: {
+        visible: {
+            type: Boolean,
+            default: false
+        },
+        form: {
+            type: Object,
+            default: () => {
+                return {};
+            }
+        }
+    },
+    data() {
+        return {
+            loading: false,
+            rules: validation(this)
+        };
+    },
+    computed: {
+        visible2: {
+            get() {
+                return this.visible;
+            },
+            set(value) {
+                this.$emit('update:visible', value);
+            }
+        },
+        actionName() {
+            return (this.form.id ? '编辑' : '添加') + '文档';
+        }
+    },
+    methods: {
+        clearValidate() {
+            this.$nextTick(() => {
+                const form = this.$refs.form;
+                form && form.clearValidate();
+            });
+        },
+
+        onClose() {
+            this.clearValidate();
+        },
+
+        onOpen() {
+            this.clearValidate();
+        },
+
+        async onSure() {
+            try {
+                await this.$refs.form.validate();
+            } catch (err) {
+                return;
+            }
+
+            try {
+                this.loading = true;
+                await createUpdateDocument(this.form);
+            } catch (err) {
+                return await error(err);
+            } finally {
+                this.loading = false;
+            }
+
+            success(`${this.actionName}成功`);
+            this.visible2 = false;
+            this.$emit('success');
+        }
+    }
+};
+</script>
--- src/services/integration.js
+++ src/services/integration.js
@@ -52,3 +52,54 @@ export async function createUpdateEduction(form) {
     });
     return res.data;
 }
+
+/**
+ * 获取文档列表数据
+ * @param pageSize
+ * @param pageNumber
+ * @param name
+ * @returns {Promise<void>}
+ */
+export async function getDocumentList(pageSize, pageNumber, name) {
+    const res = await service({
+        url: url,
+        params: {
+            pageSize,
+            pageNumber,
+            name
+        }
+    });
+    return res.data;
+}
+
+/**
+ * 删除文档
+ * @param id
+ * @returns {Promise<null|any>}
+ */
+export async function removeDocument(id) {
+    const res = await service({
+        url: `${url}/${id}`,
+        method: 'delete'
+    });
+
+    if (res.data === true) {
+        return null;
+    }
+
+    throw new Error('删除失败');
+}
+
+/**
+ * 新增编辑文档
+ * @param form
+ * @returns {Promise<null|any>}
+ */
+export async function createUpdateDocument(form) {
+    const res = await service({
+        url: `${url}/createUpdate`,
+        method: 'post',
+        data: form
+    });
+    return res.data;
+}
